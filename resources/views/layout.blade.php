335aead5616e615c9e19901c6eca0028d4f07582
NewYork96 <varger96@gmail.comgit config --global user.email varger96@gmail.comgit config --global user.email varger96@gmail.com>
2021-12-19 15:40:05 +0100

    After tags

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,12 +4,9 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="../css/app.css">
-    <title>Dream4sys Blog</title>
+   <title>Dream4sys Blog</title>
   </head>
   <body>
     <div class="container">
@@ -33,7 +30,21 @@
             <p class="fixed-top bg-success text-white col-2">{{session()->get('successDelete')}}</p>
       @endif
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+      crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+      $(document).ready(function() {
+        $('.js-example-basic-multiple').select2();
+        $('.js-example-basic-multiple').select2({
+        tags: true
+        });
+      });
+    </script>  
+
   </body>
 </html>
