36c558723ad453bd76f29e33edf1979c71494c5f
NewYork96 <varger96@gmail.comgit config --global user.email varger96@gmail.comgit config --global user.email varger96@gmail.com>
2021-12-19 23:29:08 +0100

    Raedy

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,6 +2,6 @@
   
 @section('header')
     <div class="col bg-info text-dark d-flex align-items-center d-flex justify-content-center">
-        <h1>Dream4sys Blog</h1>
+        <a href="{{route('post.index')}}" style="text-decoration: none"><h1 class="header-text text-dark">Dream4sys Blog</h1></a>
     </div>
 @endsection
